3654f92a9cf54ebdfd63c511b3e187f27593150b
Augusto Pascutti <contato@augustopascutti.com>
2010-06-30 20:52:50 -0300

    License added

--- jLabel.js
+++ jLabel.js
@@ -6,8 +6,29 @@
  * select an input it tries to get the label related to that input, and if
  * you select a
  * 
- * @todo Make a better filter when the form is submitted, for ajax inputs it will be a problem. If you only work with normal form-tags it will work like a charm
- * @author Augusto Pascutti <contato@augustopascutti.com>
+ * MIT License
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @todo Make a better filter when the form is submitted, for ajax inputs it 
+ *       will be a problem. If you only work with normal form-tags it will 
+ *       work like a charm.
+ * @author Augusto Pascutti <augusto [at] phpsp [dot] org [dot] br>
  */
 (function($) {
   $.fn.jLabel = function(settings) {
